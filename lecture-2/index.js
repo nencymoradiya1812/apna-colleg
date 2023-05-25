ca10cc119828697c1cbe79e0e91659930e0d2923
nencymoradiya1812 <nencymoradiya18@gmail.com>
2023-05-25 22:37:52 +0530

    show name in input

--- lecture-2/index.js
+++ lecture-2/index.js
@@ -1,5 +1,6 @@
 let pop=document.querySelector("button")
-pop.addEventListener("click",show)
-function show(){
-                alert("hiiii i am showing your alert");
+pop.addEventListener("click",input)
+function input(){
+                  let  name =prompt("enter name of student");
+                  name.tectcontent="roll no.1" + name;
 }
